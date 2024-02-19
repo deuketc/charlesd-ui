45d92ddda655ac5d4885d60ca523dd9faad1f8cf
TJ07\charl <charles.deukett@hotmail.com>
2024-02-19 19:35:53 +1300

    Adding intro animation to coffee cup

--- src/components/Coffee/Coffee.tsx
+++ src/components/Coffee/Coffee.tsx
@@ -1,14 +1,45 @@
 import { OrbitControls } from '@react-three/drei';
 import { useGLTF } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { useRef } from 'react';
+import { useRef, useLayoutEffect, useEffect } from 'react';
+import { gsap } from 'gsap';
 
 const Coffee = () => {
   const { nodes, materials } = useGLTF('/coffee_mug-uv-map-compressed.glb');
-  const cup = useRef();
-  useFrame(({ clock }) => {
-    cup.current.rotation.y = clock.getElapsedTime() / 5;
-  });
+  const cup = useRef(null);
+  // useFrame(({ clock }) => {
+  // setTimeout(() => {
+  //   cup.current.rotation.y = clock.getElapsedTime() / 5;
+  // }, 1);
+  // });
+
+  useEffect(() => {
+    console.log('cup.current', cup.current);
+    let ctx = gsap.context(() => {
+      gsap.to(cup.current.scale, {
+        y: 3.5,
+        x: 3.5,
+        z: 3.5,
+        duration: 1,
+      });
+
+      gsap.to(cup.current.rotation, {
+        y: 2.9,
+        duration: 1,
+      });
+
+      gsap.to(cup.current.rotation, {
+        y: 9.18,
+        duration: 23,
+        delay: 1,
+        repeat: -1,
+        ease: 'none',
+      });
+
+      return () => ctx.revert();
+    });
+  }, []);
+
   return (
     <>
       <OrbitControls makeDefault />
@@ -16,7 +47,7 @@ const Coffee = () => {
       <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1.5} />
 
-      <group scale={3.5} ref={cup} dispose={null}>
+      <group scale={6} ref={cup} dispose={null}>
         <mesh
           castShadow
           receiveShadow
