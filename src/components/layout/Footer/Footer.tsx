800facdc92fbaefc9339cf77b8942ea911e1fcbd
TJ07\charl <charles.deukett@hotmail.com>
2025-07-19 21:09:37 +1200

    footer completed and copy tweaks

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,53 +1,56 @@
 import styles from './Footer.module.scss';
-import LogoTwitter from '../../../assets/svg/LogoTwitter';
-import LogoPinterest from '../../../assets/svg/LogoPinterest';
-import LogoLinkedin from '../../../assets/svg/LogoLinkedin';
-import LogoInstagram from '../../../assets/svg/LogoInstagram';
-import LogoYoutube from '../../../assets/svg/LogoYoutube';
-import LogoFacebook from '../../../assets/svg/LogoFacebook';
-import LogoGithub from '../../../assets/svg/LogoGithub';
-import LogoCodepen from '../../../assets/svg/LogoCodepen';
+import {
+  faCodepen,
+  faGithub,
+  faLinkedin,
+  faYoutube,
+  faInstagram,
+  faXTwitter,
+  faFacebook,
+  faPinterest,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const footerLinks = [
   {
     label: 'LinkedIn',
     href: 'https://nz.linkedin.com/in/charles-deukett-718bba73',
-    img: <LogoLinkedin />
+    icon: faLinkedin,
   },
   {
     label: 'Youtube',
     href: 'https://www.youtube.com/user/deuketc',
-    img: <LogoYoutube />,
+    icon: faYoutube,
   },
   {
     label: 'Instagram',
     href: 'https://www.instagram.com/charlesdeukett/',
-    img: <LogoInstagram />,
+    icon: faInstagram,
   },
   {
-    label: 'Twitter',
-    href: 'https://twitter.com/CharlesDeukett',
-    img: <LogoTwitter />,
+    label: 'X',
+    href: 'https://x.com/CharlesDeukett',
+    icon: faXTwitter,
   },
   {
     label: 'Facebook',
     href: 'https://www.facebook.com/charles.deukett',
-    img: <LogoFacebook />,
+    icon: faFacebook,
   },
   {
     label: 'Pinterest',
     href: 'https://www.pinterest.nz/charlesdeukett/',
-    img: <LogoPinterest />,
+    icon: faPinterest,
   },
   {
     label: 'Github',
     href: 'https://github.com/deuketc',
-    img: <LogoGithub />,
+    icon: faGithub,
   },
   {
     label: 'Codepen',
     href: 'https://codepen.io/charlesdeukett',
-    img: <LogoCodepen />,
+    icon: faCodepen,
   },
 ];
 
@@ -56,13 +59,20 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.footer__wrapper}>
         <nav className={styles.footer__navigation}>
-          <h4 className={styles.footer__header}>Follow me</h4>
+          <div className={styles.footer__header_wrapper}>
+            <h4 className={styles.footer__header}>Follow me</h4>
+            <p className={styles.footer__header_sub}>
+              Let's connect and build something cool together.
+            </p>
+          </div>
           <ul className={styles.footer__list}>
             {footerLinks.map(footerLink => {
               return (
                 <li key={footerLink.label} className={styles.footer__list_item}>
                   <a
-                    className={styles.footer__list_item_link}
+                    className={`${styles[footerLink.label.toLowerCase()]} ${
+                      styles.footer__list_item_link
+                    }`}
                     target="_blank"
                     href={footerLink.href}
                     rel="noreferrer"
@@ -70,7 +80,7 @@ const Footer = () => {
                     <span className={styles.footer__list_item_label}>
                       {footerLink.label}
                     </span>
-                    {footerLink.img}
+                    <FontAwesomeIcon icon={footerLink.icon} />
                   </a>
                 </li>
               );
@@ -78,7 +88,8 @@ const Footer = () => {
           </ul>
         </nav>
         <p className={styles.footer__copyright}>
-          &copy; {new Date().getFullYear()}
+          <span>&copy; {new Date().getFullYear()} Charles Deukett</span>
+          <span>Made with love & caffeine</span>
         </p>
       </div>
     </footer>
